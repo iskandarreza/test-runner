360ce1cba70884a0e23d92c4fa1cdf5cef581291
Iskandar Reza <iskandarreza@gmail.com>
2022-10-09 13:55:57 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const puppeteer = require('puppeteer')
 const appRoot = require('app-root-path');
-const path = require('path')
 const fs = require('fs')
 
 /**
  * Launch a browser, go to a page, and run a script on that page.
  * @param {string} pageUrl - The URL of the page you want to test.
- * @param {string} codePath - The path to the file that contains the code you want to run.
+ * @param {string} codePath - The absolute path to the file that contains the code you want to run.
  */
 exports.launchTest = async (pageUrl, codePath) => {
-    // const file_path = `${path.join(__dirname, codePath)}`
+    if(Array.from(codePath)[0] !== '/') {
+        codePath = '/' + codePath
+    }
     const file_path = `${appRoot + codePath}`
     const file_content = fs.existsSync(file_path) ? fs.readFileSync(file_path, 'utf8') : ''
     const browser = await puppeteer.launch({
-        headless: false, slowMo: 250, args: ['--window-size=1920,1080', '--allow-file-access-from-files'],
+        headless: false, slowMo: 250, 
+        args: [
+            '--window-size=1920,1080', 
+            '--allow-file-access-from-files'
+        ],
         defaultViewport: {
             width: 1920,
             height: 1080
         }
     })
-    
-    console.info('Is this path to the test code correct? -- ', file_path)
 
     const page = await browser.newPage()
 
     await page.goto(pageUrl, { waitUntil: 'domcontentloaded' })
-
-    await page.evaluate(((file_content) => {
-        file_content
-    }, file_content))
+    
+    if (file_content.length <= 0) {
+        console.info('INFO: file is empty. Is the path to the file correct?')
+        console.info('Path: ', file_path )
+    } else {
+        await page.evaluate(((file_content) => {
+            file_content
+        }, file_content))
+        .then(() => { console.info('INFO: code was evaluated')})
+    }
 }
