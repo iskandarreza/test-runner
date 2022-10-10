23e9453b01112a54e96f99956923da12b5a7a450
Iskandar Reza <iskandarreza@gmail.com>
2022-10-09 23:53:26 -0700

    Update runner.js

--- unit-testing/runner.js
+++ unit-testing/runner.js
@@ -29,24 +29,4 @@ const runTests = () => {
     })
 }
 
-// mocha.addFile(`${path.join(__dirname, 'bootstrap.js')}`)
-
-// // Add each .js file to the mocha instance
-// fs.readdirSync(testDir).filter(function (file) {
-//     // Only keep the .js files
-//     return file.substr(-3) === '.js'
-
-// }).forEach(function (file) {
-//     mocha.addFile(
-//         path.join(testDir, file)
-//     )
-// })
-
-// mocha.timeout(15000);
-
-// // Run the tests.
-// mocha.run(function (failures) {
-//     process.exitCode = failures ? 1 : 0  // exit with non-zero status if there were failures
-// })
-
 exports.runTests = runTests
